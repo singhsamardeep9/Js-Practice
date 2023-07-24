acdc26bf0580f645ccd062c120b88b36d5774636
singhsamardeep9 <56290954+singhsamardeep9@users.noreply.github.com>
2023-07-24 12:34:19 +0000

    learned dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+ "use strict";// treats all js code as newer version
+
+//  alert(3+3) since we are using nodejs and not browser
+let name = "Samar"
+let age = 22
+let isLoggedIn = false
+let state = null
+//number--> range 2^53
+//bigint
+//string-->""
+//boolean--> true/false
+//null--> stand alone value(it is a data type) means khaali hai, we can also assign it to a variable
+//undefined--> when value is not initialised or defined to a variable
+//symbol--> defines uniqueness
+
+//object--> v.Imp
+
+//typeof function--> returns the type of value stored in a variable
+// it returns the type for a particular value or can return for variable also
+console.log(typeof "Samar")
+console.log(typeof age)
+//typeof for null returns object
+console.log(typeof null);
+//typeof for undefined returns undefined
+console.log(typeof undefined);
