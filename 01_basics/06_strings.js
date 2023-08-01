e74b207281cf7bbde0757154a5480399949fe90e
singhsamardeep9 <56290954+singhsamardeep9@users.noreply.github.com>
2023-08-01 14:50:20 +0530

    Update 06_strings.js

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -1,5 +1,5 @@
 /*
-In JS string can be initialised using both single quotes('') anf double quotes("") and for
+In JS string can be initialised using both single quotes('') and double quotes("") and for
 concatenation of stings we can simply use (+ or ,) opertors but this is an older way to 
 perform operations on strings the newer js has efficient and better ways for string operations
 */
@@ -7,7 +7,8 @@ perform operations on strings the newer js has efficient and better ways for str
 const name = "Samar"
 const score = 50;
 
-console.log(name + score + "runs"); //this is an older way
+//this is an older way
+console.log(name + score + "runs"); //output--> Samar50runs 
 
 /*
 The newer method for string operations is js is usage of backticks(``) that helps in string
@@ -21,15 +22,18 @@ yielding a result in which the placeholders are replaced with their correspondin
 these placeholders are created using ${} 
 */
 
-console.log(`Hello my name is ${name} and i scored ${score} runs`);//this syntax is modern and better
+//this syntax is modern and better
+console.log(`Hello my name is ${name} and i scored ${score} runs`); //output--> Hello my name is Samar and i scored 50 runs
 
 //another way for string declaration
 
 
 const userName = new String('Samar')
+
 //the typeof function for string declared using this method returns object
 console.log(typeof userName)//output--> object
 
+console.log(userName);
 /* object output in key:value pairs
 userName = {
     0 : S,
@@ -47,7 +51,7 @@ console.log(userName[0])//output--> S
 //acessing the prediefined functions 
 console.log(userName.length)//output--> 5
 console.log(userName.toUpperCase()); /*output--> SAMAR but this function does not change the orignal 
-value of the string userName sincce it is of ptimitive type*/
+value of the string userName since it is of primitive type*/
 
 console.log(userName.charAt(2));//output--> m
 console.log(userName.indexOf('r')); //output--> 4
@@ -58,8 +62,8 @@ const newStr = gameName.substring(0,4)
 console.log(newStr)//output-->Sama
 
 /*Now suppose i want to convert my gameName into an array and devide it on the basis of 
-hyphen(-) then split method is used*,it is important to note that split can also be dine in the basis
-some character/whitespace/symbol*/
+hyphen(-) then split method is used, it is important to note that split can also be done in the basis
+of some character/whitespace/symbol */
 
 console.log(gameName.split('-')); //output--> ['Samar','9','Singh']
 
@@ -70,16 +74,16 @@ otherise it works the same as substring*/
 const newStr1 = gameName.slice(-12,4)
 console.log(newStr1) //output--> ama
 
-//trim method--> removes all the extra white spaces in the start and end
+//trim method--> removes all the extra white spaces in the start and end of the string
 const newStr2 = "   Samar    "
-console.log(newStr2.trim()) //output--> "Samar" without white spaces
+console.log(newStr2.trim()) //output--> "Samar" (without white spaces)
 
 //replace method--> 
 const url = "https://Samar.com/Samar%20Singh"
 console.log(url.replace('%20','-'));//output--> https://Samar.com/Samar-Singh
 
 /*includes method is used to check whether my string includes another given string passed in 
-arguement or not*/
+arguement or not */
 
 console.log(url.includes('Samar'))//output--> true
 console.log(url.includes('ameer'))//output--> false
