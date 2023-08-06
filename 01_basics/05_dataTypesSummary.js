1c7bf2c01050530e9aa21cab2b793338458b1542
singhsamardeep9 <56290954+singhsamardeep9@users.noreply.github.com>
2023-08-07 03:15:58 +0530

    Update 05_dataTypesSummary.js

--- 01_basics/05_dataTypesSummary.js
+++ 01_basics/05_dataTypesSummary.js
@@ -1,17 +1,17 @@
 /*
 JS documentation states that there are only two types based on how they are stored in memory
 
-1.Primitives(7types)--> String,Number,Boolean,null,undefined,Symbol,BigInt(a copy of value 
-    is passed instead of actual reference to the memory and changes made does not change
-    the orignal value)
+1.Primitives(7 types)--> String,Number,Boolean,null,undefined,Symbol,BigInt(a copy of value 
+    is passed instead of actual reference to the memory and changes made in the copy does 
+    not change the orignal value)
 
-2.Non Primitives/Reference(3types)--> Arrays,objects,functions(actual reference to the memory
+2.Non Primitives/Reference(3 types)--> Arrays,objects,functions(actual reference to the memory
     is passed and changes made to them changes the orignal values)
-
 */ 
 
 /*NOTE: It is to remember that a variable delared with const keyword should always be 
-assigned some value otherwise without initialization it would result in error*/
+assigned some value otherwise without initialization it would result in an error as:
+(Uncaught SyntaxError: Missing initializer in const declaration)*/
 
 const  score = 100.3 //Number type
 const email = "singhsamardeep9@gmail.com" //string type
@@ -31,19 +31,19 @@ console.log(userId == anotherUserId); //output-->false
 //Arrays
 const heroes = ["spiderman","superman","Thor"]
 
-//objects ar stored in the form of key value pairs
+//objects are stored in the form of key:value pairs
 const myObj = {
     name:"Samar",
     age:"22",
 }
 
-/*functions in js are simply declared by using "function" keyword 
-and can simply be assigned to a variable as shown below*/
+/* Functions in js are simply declared by using "function" keyword 
+and can simply be assigned to a variable as shown below: */
 let hello = function(){
     console.log("Hello World");
 }
 
-/*The return type after using typeof function for non primitive values gives objeect as
+/* The return type after using typeof function for non primitive values gives object as
 an output:
 Object--> Object
 Array--> Object
@@ -57,6 +57,7 @@ function--> Object function
 
 let  myName = "Samardeep"
 let anotherName = myName;
+anotherName = "Samar";
 
 /*
 imp concept--> the value of variable myName does not change since a copy of value is passed to
@@ -64,22 +65,19 @@ variable anotherName and not the actual reference of the memory is passed, and e
  in variable anotherName the orignal value of myName is not changed
 */
 
-anotherName = "Samar";
-
 console.log(myName); //output-->Samardeep
 console.log(anotherName);//output-->Samar
 
-/*
-In the case of Non Primitive types a value changed by userTwo changes the value in userOne since
-the actual reference of userOne is passed to userTwo
-*/
 
 let userOne = {
     emailId : "email@123",
     upiId : "user@ybl"
 }
-
 let userTwo = userOne;
+
+/* In the case of Non Primitive types a value changed by userTwo changes the value in userOne since
+the actual reference of userOne is passed to userTwo */
+
 userTwo.emailId = "email321@google.com"
 //here the value of emailId in userOne also changes as that of userTwo
 
